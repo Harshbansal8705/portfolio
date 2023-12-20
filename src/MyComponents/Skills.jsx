70d7f19b4b5e9695618027d62070ddc497823aa6
Harshbansal8705 <harshbansal8705@gmail.com>
2023-12-20 13:53:25 +0530

    Added new Skills - Next and Tailwind

--- src/MyComponents/Skills.jsx
+++ src/MyComponents/Skills.jsx
@@ -5,8 +5,8 @@ export default function Skills() {
   return (
     <section className="skills" id="skills">
         <h1>Skills</h1>
-        <SkillsRow type={"Programming Languages"} skills={["Python", "C", "C++", "HTML", "CSS", "JavaScript"]} />
-        <SkillsRow type={"Frameworks"} skills={["Numpy", "Pandas", "Django", "Flask", "OpenCV", "ReactJs", "NodeJs"]} />
+        <SkillsRow type={"Languages"} skills={["Python", "C", "C++", "HTML", "CSS", "JavaScript"]} />
+        <SkillsRow type={"Frameworks"} skills={["Numpy", "Pandas", "Django", "Flask", "OpenCV", "ReactJs", "NodeJs", "NextJs", "TailwindCSS"]} />
         <SkillsRow type={"Machine Learning"} skills={["Scikit Learn", "Algorithms"]} />
         <SkillsRow type={"Tools"} skills={["Git", "Github", "MongoDB"]} />
         <SkillsRow type={"Soft Skills"} skills={["Leadership", "Event Management", "Speaking", "Time Management"]} />
