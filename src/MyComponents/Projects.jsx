e44c699907aae43ac051129e32738421b1daa176
Harsh Bansal <harshbansal8705@gmail.com>
2024-03-24 00:08:45 +0530

    Added ECommerce website in projects

--- src/MyComponents/Projects.jsx
+++ src/MyComponents/Projects.jsx
@@ -3,12 +3,23 @@ import Project from './Project'
 import VirtualPaint from "../static/images/VirtualPaint.png"
 import Pypi from "../static/images/pypi.jpeg"
 import DigitRecognizer from "../static/images/DigitRecognizer.png"
+import ECommerce from "../static/images/ECommerce.jpg"
 
 export default function Projects() {
     return (
         <section className="projects" id="projects">
             <h1 className='invisible-fade-up'>Projects</h1>
             <div className="projects-grid grid-invisible-fade-up">
+                <Project
+                    image={ECommerce}
+                    title="E-Commerce Website"
+                    tags={["Reactjs", "Nodejs", "Expressjs", "MongoDB", "Redux"]}
+                    description="This is a comprehensive E-Commerce website created with MERN Stack, integrating user authentication, advanced cart and product management, optimizing the product list with brand and category filters."
+                    links={{
+                        "github": "https://github.com/Harshbansal8705/Digit-Recognize",
+                        "visit": "https://ecommerce-steel-eight.vercel.app/"
+                    }}
+                />
                 <Project
                     image={VirtualPaint}
                     title="Virtual Paint App"
@@ -33,7 +44,7 @@ export default function Projects() {
                     tags={["Machine Learning", "Python", "Scikit Learn", "Data Science"]}
                     description="Leveraging XGBoost algorithm, I constructed a digit recognition model that attained an impressive 98% accuracy on the MNIST dataset."
                     links={{
-                        "github": "https://github.com/Harshbansal8705/Digit-Recognize",
+                        "github": "https://github.com/Harshbansal8705/Digit-Recognizer",
                         "kaggle": "https://www.kaggle.com/harshbansalcoder/digit-recognizer"
                     }}
                 />
