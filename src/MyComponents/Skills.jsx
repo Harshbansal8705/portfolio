09c968c1080ba2987caefe450fe6093acf1a2d17
Harsh Bansal <harshbansal8705@gmail.com>
2024-11-30 19:45:43 +0530

    Updated Skills

--- src/MyComponents/Skills.jsx
+++ src/MyComponents/Skills.jsx
@@ -7,7 +7,7 @@ export default function Skills() {
         <h2>Skills</h2>
         <SkillsRow type={"Languages"} skills={["Python", "C", "C++", "JavaScript", "TypeScript"]} />
         <SkillsRow type={"Frameworks"} skills={[ "ReactJs", "NodeJs", "NextJs", "Django", "Flask", "OpenCV", "Numpy", "Pandas", "Sklearn", "Tensorflow"]} />
-        <SkillsRow type={"Tools"} skills={["Git", "Github", "MongoDB", "Docker"]} />
+        <SkillsRow type={"Tools"} skills={["Git", "Github", "MongoDB", "PostgreSQL", "Docker", "AWS"]} />
         <SkillsRow type={"Soft Skills"} skills={["Leadership", "Event Management", "Speaking", "Time Management"]} />
         <SkillsRow type={"Others"} skills={["Redux", "Web Socket"]} />
     </section>
